4b802a0d0ff0bbe468c1eeb13bdfa34a5712ee37
John <80493630+Kimewaza@users.noreply.github.com>
2021-06-24 23:15:16 -0400

    Update NameSetterAndGetter.java

--- Integration/src/integrationpackage/NameSetterAndGetter.java
+++ Integration/src/integrationpackage/NameSetterAndGetter.java
@@ -1,19 +1,35 @@
 package integrationpackage;
-//John Nguyen
+
+// John
 
 
+/**
+ * Class that sets and and gets the arguments that are passed into the parameters.
+ * @author John Nguyen
+ *
+ */
 public class NameGetterAndSetter {
-	private String name;
-	private String month;
-	private String day;
-	private String year;
-	
-	public NameGetterAndSetter(String n, String m, String d, String y) {
-		name = n;
-		month = m;
-		day = d;
-		year = y;
-		System.out.println("Hello " + n + "\n Its " + m + " - " + d + " - "+ y + ".");
-	}
-	
+  
+  private String name;
+  private String month;
+  private String day;
+  private String year;
+
+
+  /**
+   * This constructor takes all the arguments and sets it equal to its respective variables.
+   * It outputs the print statement with the arguments assigned. 
+   * @param n Takes the argument n and is assigned to name.
+   * @param m Takes the argument m and is assigned to month.
+   * @param d Takes the argument d and is assigned to day.
+   * @param y Takes the argument y and is assigned to year.
+   */
+  public NameGetterAndSetter(String n, String m, String d, String y) {
+    this.name = n;
+    this.month = m;
+    this.day = d;
+    this.year = y;
+    System.out.println("Hello " + name + "\nThis program is finished at " + month + " - " + day + " - " + year + ".");
+  }
+
 }
